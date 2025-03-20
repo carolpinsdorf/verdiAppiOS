c8826c4780aa6fa254c42126a854908b1f497ac9
carolpinsdorf <carolinapinsdorf@gmail.com>
2025-03-20 15:49:53 -0300

    HistoryView estilizacão terminada

--- verdi/Views/HistoryView.swift
+++ verdi/Views/HistoryView.swift
@@ -11,17 +11,44 @@ struct HistoryView: View {
     @EnvironmentObject var viewModel: RouteViewModel
     
     var body: some View {
-        NavigationView {
-            List(viewModel.routes) { route in
-                VStack(alignment: .leading) {
-                    Text("\(route.origin) → \(route.destination)")
-                        .font(.headline)
-                    Text(route.date, style: .date)
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
+        NavigationStack {
+            ZStack {
+                VStack(spacing: 25) {
+                    Image("logo-sem-fundo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                    
+                    Text("Histórico")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Theme.darkAccentColor)
+                        .padding(.top, 20)
+                    
+                    List(viewModel.routes) { route in
+                        VStack(alignment: .leading) {
+                            Text("\(route.origin) → \(route.destination)")
+                                .font(.headline)
+                                .foregroundColor(Theme.textColor) // Usando a cor do tema
+                            Text(route.date, style: .date)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                
+                        }
+                        .padding()
+                        .background(Theme.darkAccentColor) // Fundo com cor do tema
+                        .cornerRadius(10)
+                    }
+                    .listStyle(PlainListStyle())
                 }
             }
-            .navigationTitle("Histórico")
+            .navigationTitle("")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
+}
+
+#Preview {
+    HistoryView()
+        .environmentObject(RouteViewModel())
 } 
